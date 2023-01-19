874be288ea919842259380fa274560e1b6ec8b0b
Bommezijn <nathan@mandelo.nl>
2023-01-20 00:55:35 +0100

    cleanup: removal of comments

--- artboards/sketch01.js
+++ artboards/sketch01.js
@@ -14,12 +14,12 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const cx = width * 0.5; //center of circle
+    const cx = width * 0.5;
     const cy = height * 0.5;
 
     const w = width * 0.01;
     const h = height * 0.1;
-    let x,y; // going to be modified in loop
+    let x,y;
 
     const num = 24; //number of squares
     const radius = width * 0.3; //size of radius
@@ -29,12 +29,11 @@ const sketch = () => {
       const slice = degToRad(360 / num);
       const angle = slice * index;
 
-      x = radius * Math.sin(angle); // or cx/cy + radius * Math.sin(angle)
-      y = radius * Math.cos(angle);
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
 
 
       context.save();
-      context.translate(cx,cy);
       context.translate(x,y);
       context.rotate(-angle);
 
@@ -45,24 +44,6 @@ const sketch = () => {
       context.restore();
     }
 
-/*  
-// save the current state of the context and push on stack
-    context.save();
-    // positioning the context aka rect to a location on the canvas
-    context.translate(x,y);
-    // rotating the context aka rect. Rotates from the top left corner of canvas. is in radians instead of deg
-    // context.rotate(0.3);
-    //hassle to do it constantly, make a func for it
-    // context.rotate(45 / 100 * Math.PI);
-    context.rotate(degToRad(45)); //hassle to do it constantly, make a func for it
-
-    context.fillStyle = 'black';
-    context.beginPath();
-    context.rect(0,0,w,h);
-    context.fill();
-    // restore last saved state of the context and pop off stack
-    context.restore();
-*/
   };
 };
 
